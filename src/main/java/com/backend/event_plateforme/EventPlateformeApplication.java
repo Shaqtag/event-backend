c232ece6910de6a062a011494eda94b378d7992b
shaquille <shaquillempeck@gmail.com>
2025-05-20 21:22:47 +0100

    Springboot Backend

---
+++ src/main/java/com/backend/event_plateforme/EventPlateformeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.event_plateforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlateformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlateformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/event_plateforme/EventPlateformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.event_plateforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlateformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
