d724dd6432bec51b8283a5f6871880404790b8f5
RivaldoDolce <rivaldofer2@gmail.com>
2025-06-04 04:41:56 +0200

    reglage du fichier pom.xml de prestation et ajout de dcouverte

--- services/prestation_service/prestation_service/src/main/java/com/rivaldo_service/prestation_service/PrestationServiceApplication.java
+++ services/prestation_service/prestation_service/src/main/java/com/rivaldo_service/prestation_service/PrestationServiceApplication.java
@@ -2,6 +2,7 @@ package com.rivaldo_service.prestation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
